7fa1bea9d0804653e08834aca8735d9bd556088a
14569 <165989521+kk14569@users.noreply.github.com>
2024-07-16 13:39:44 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -34,4 +34,32 @@ return [
             })();</script>';
         })
         ->css(__DIR__.'/less/forum.less'),  
+    (new Extend\Frontend('admin'))
+        ->content(function (Document $document) {
+            $document->head[] = '<script>(function () {
+                var r = getComputedStyle(document.querySelector(":root"));
+                function changeTitleColor() {
+                    if (r.getPropertyValue("--colored-titlebar") === "false") {
+                        var m = document.querySelector(`meta[name="theme-color"]`);
+                        m.content = r.getPropertyValue("--header-bg");
+                    };
+                }    
+                changeTitleColor();
+                if (r.getPropertyValue("--colored-titlebar") === "false") new Promise(function (resolve) {
+                    var id = setInterval(function () {
+                        if (flarum) {
+                            if (flarum.extensions) {
+                                clearInterval(id);
+                                resolve();
+                            }
+                        }
+                    }, 500)
+                }).then(function () {
+                    if (flarum.extensions["fof-nightmode"]) {
+                        document.addEventListener("fofnightmodechange", changeTitleColor);
+                    }
+                });
+            })();</script>';
+        })
+        ->css(__DIR__.'/less/admin.less'),  
 ];
